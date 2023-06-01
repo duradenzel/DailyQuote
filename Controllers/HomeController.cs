9978c51c5e5ecec3b9b872bf337c94cf4f2cfac3
Denzel Huijbers <huijbers15@outlook.com>
2023-06-01 11:36:26 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using DailyQuote.Models;
+using Logic;
+using Logic.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DailyQuote.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        private readonly QuoteService _quoteService = new();
+        public IActionResult Index()
+        {
+            Quote q = _quoteService.GetQuote();
+            return View(q);
+        }
+
+       
+
+    }
+}
