fe93da361dceae18d5496793a753ff67011fb71c
Denzel Huijbers <huijbers15@outlook.com>
2023-06-13 10:40:53 +0200

    fixed issue where logic layer was ignored

---
+++ Logic/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    public class Quote
+    {
+        public string Text { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Logic/QuoteService.cs
@@ -0,0 +1,28 @@
+﻿using Logic.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Reflection;
+
+namespace Logic
+{
+    public class QuoteService
+    {
+        private static List<Quote> quotes;
+
+        public  Quote GetQuote()
+        {
+            if (quotes == null)
+            {
+                var jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "quotes.json");
+                var json = File.ReadAllText(jsonPath);
+                quotes = JsonConvert.DeserializeObject<List<Quote>>(json);
+            }
+
+            var currentDate = DateTime.Now.Date;
+            var totalQuotes = quotes.Count;
+           
+            var index = currentDate.DayOfYear % totalQuotes;
+            return quotes[index];
+        }
+    }
+}
